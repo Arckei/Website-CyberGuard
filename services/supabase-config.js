5af911d1eb72719c283340c1aed6f1354d4282f9
ARCKEI <keithwilsonplays@gmail.com>
2026-09-13 22:10:36 +0800

    remove supabase to the website

--- services/supabase-config.js
+++
@@ -1,6 +0,0 @@
-export const supabaseStorageConfig = {
-  enabled: false,
-  url: "https://vumqubqukdzasyhccske.supabase.co",
-  anonKey: "sb_publishable_vYWcpTS2erKIEJZlcl3PhQ_jw0Q8MeT",
-  bucket: "cyberguard-lessons"
-};
